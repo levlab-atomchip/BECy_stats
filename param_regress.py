170b3c518d6d3dbb685978e1b0af9f9fc3212e64
Richard Turner <rwturner @ stanford.edu>
2013-08-22 21:04:23 -0700

    added csvwriter and stats to param_regress

--- param_regress.py
+++ param_regress.py
@@ -1,7 +1,10 @@
 import CloudImage
 import glob
 import matplotlib.pyplot as plt
-dir = 'C:\\ImagingSave\\statistics\\loadmot_varyLoadTime\\2013-08-22\\'
+from scipy import stats
+import csv
+dir = 'C:\\Users\\Levlab\\Documents\\becy_stats\\statistics\\loadmot_varyLoadTime\\2013-08-22\\'
+ContParName = None
 
 imagelist = glob.glob(dir + '*.mat')
 
@@ -14,14 +17,30 @@ imgind = 1
 for img in imagelist:
     thisimg = CloudImage.CloudImage(img)
     # print(thisimg.CurrContPar)
-    param_vals.append(thisimg.CurrContPar)
+    param_vals.append(thisimg.CurrContPar[0])
     # print(param_vals)
     numbers.append(thisimg.getAtomNumber())
     print('Processed %d out of %d images'%(imgind, numimgs))
     imgind += 1
+    
+ContParName = thisimg.ContParName
 
+    
+outputfile = dir + ContParName + '.csv'
+with open(outputfile, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow((ContParName, 'Number'))
+    rows = zip(param_vals, numbers)
+    writer.writerows(rows)
+    
+slope, intercept, r_value, p_value, std_err = stats.linregress(param_vals, numbers)
+
+print("Slope: %f"%slope)
+print("Intercept: %f"%intercept)
+print("Standard Error: %f"%std_err)
+    
 plt.plot(param_vals,numbers,'.')
-plt.xlabel(thisimg.ContParName)
+plt.xlabel(ContParName)
 plt.ylabel('Atom Number') 
 plt.show()
 
