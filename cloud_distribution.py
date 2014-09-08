added BECphysics for width calculation added custom_fit_switch added image rotation correction fixed negative amplitude fitting problem

cloud_distribution.py
@@ -22,17 +22,23 @@ import pprint
 import win32gui
 from win32com.shell import shell, shellcon
 
-DEBUG_FLAG = True
+DEBUG_FLAG = False
 LINEAR_BIAS_SWITCH = False
-FLUC_COR_SWITCH = False
+FLUC_COR_SWITCH = True
 OFFSET_SWITCH = True
-FIT_AXIS = 0; # 0 is x, 1 is z
+FIT_AXIS = 1; # 0 is x, 1 is z
+CUSTOM_FIT_SWITCH = False
+
+custom_fit_window = [162,800,60,485]
 
 def lifetime_func(x, a, b, c):
     return a * np.exp(-b * x) + c
+    
+def freq_func(t, omega, amplitude, offset, phase):
+    return offset + amplitude*np.sin(omega*t + phase)
+            
 
 class CloudDistribution(object):
-
     '''class representing distributions of parameters over many images'''
 
     def __init__(self, directory=None):
@@ -111,6 +117,8 @@ class CloudDistribution(object):
 
     def get_gaussian_params(self, file, **kwargs):
         this_img = cloud_image.CloudImage(file)
+        if CUSTOM_FIT_SWITCH:
+            this_img.truncate_image(*custom_fit_window)
         gaussian_params = \
                     this_img.get_gaussian_fit_params(**kwargs)
         gaussian_params['timestamp'] = this_img.timestamp()
@@ -409,7 +417,29 @@ class CloudDistribution(object):
         plt.xlabel(self.cont_par_name)
         plt.ylabel('Atom Number')
         plt.show()
+    
+    def trap_freq(self):
+        '''Fit a position to a sine function to determine trap frequency'''
+        if self.cont_par_name not in self.dists.keys():
+            self.control_param_dist()
+        
+        times = np.array(self.dists[self.cont_par_name])
+        positions = np.array(self.dists['position_z'])
 
+        pguess = np.array([2*math.pi*5e2, 0, 0, 0])
+        popt, pcov = curve_fit(freq_func, times, positions, pguess)
+        
+        print '\nRegression of position against ' + self.cont_par_name
+        print("Frequency: %2.2f Hz"%(popt[0] / 2 / math.pi))
+        print("Sigma: %2.2f Hz"%math.sqrt(pcov[0][0]))
+
+        plt.plot(times, positions, '.')
+        time_axis = np.linspace(np.min(times), np.max(times))
+        plt.plot(time_axis, freq_func(time_axis, *popt))
+        plt.xlabel(self.cont_par_name)
+        plt.ylabel('Z Position')
+        plt.show()
+        
     def kmeans(self, var1, var2, num_clusters=2):
         '''Perform a common clustering algorithm'''
         if var1 not in self.dists.keys():

cloud_image.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import os
 from math import sqrt
+from scipy.ndimage import rotate
 
 
 DEBUG_FLAG = False
@@ -21,14 +22,14 @@ def find_nearest(array, value):
     idx = (np.abs(array-value)).argmin()
     return [array[idx], idx]
 
-def gaussian_1d(x, A, mu, sigma, offset, slope):
+def gaussian_1d(x, Asqrt, mu, sigma, offset, slope):
     '''fitting function for 1D gaussian plus offset and line'''
-    return A*np.exp(-1.*(x-mu)**2./(2.*sigma**2.)) + offset + \
+    return Asqrt**2*np.exp(-1.*(x-mu)**2./(2.*sigma**2.)) + offset + \
                                                     slope*np.array(x)
 
-def gaussian_1d_noline(x, A, mu, sigma, offset):
+def gaussian_1d_noline(x, Asqrt, mu, sigma, offset):
     '''fitting function for 1D gaussian plus offset'''
-    return A*np.exp(-1.*(x-mu)**2./(2.*sigma**2.)) + offset
+    return Asqrt**2*np.exp(-1.*(x-mu)**2./(2.*sigma**2.)) + offset
 
 def gaussian_2d(xdata,
                 A_x,
@@ -51,7 +52,7 @@ def fit_gaussian_1d(image):
     max_loc = np.argmax(image)
     [_, half_max_ind] = find_nearest(image, max_value/2.)
     hwhm = 1.17*abs(half_max_ind - max_loc) # what is 1.17???
-    p_0 = [max_value, max_loc, hwhm, 0., 0.] #fit guess
+    p_0 = [np.sqrt(max_value), max_loc, hwhm, 0., 0.] #fit guess
     xdata = np.arange(np.size(image))
 
     coef, _ = curve_fit(gaussian_1d, xdata, image, p0=p_0)
@@ -64,7 +65,7 @@ def fit_gaussian_1d_noline(image):
     max_loc = np.argmax(image)
     [_, half_max_ind] = find_nearest(image, max_value/2.)
     hwhm = 1.17*abs(half_max_ind - max_loc) # what is 1.17???
-    p_0 = [max_value, max_loc, hwhm, 0.] #fit guess
+    p_0 = [np.sqrt(max_value), max_loc, hwhm, 0.] #fit guess
     xdata = np.arange(np.size(image))
 
     coef, _ = curve_fit(gaussian_1d_noline, xdata, image, p0=p_0)
@@ -135,7 +136,12 @@ class CloudImage(object):
 
         self.trunc_win_x = self.hfig_main.calculation.truncWinX
         self.trunc_win_y = self.hfig_main.calculation.truncWinY
-
+        
+        if self.image_rotation != 0:
+            self.atom_image = rotate(self.atom_image, self.image_rotation)
+            self.light_image = rotate(self.light_image, self.image_rotation)
+            self.dark_image = rotate(self.dark_image, self.image_rotation)
+        
         self.atom_image_trunc = \
         self.atom_image[self.trunc_win_y[0]:self.trunc_win_y[-1],
                         self.trunc_win_x[0]:self.trunc_win_x[-1]]
@@ -397,7 +403,7 @@ class CloudImage(object):
             ax3 = fig.add_subplot(133)
             ax3.imshow(od_image)
             plt.show()
-            
+            print "Fitting window: (%d, %d) to (%d, %d)"%(self.trunc_win_x[0], self.trunc_win_y[0], self.trunc_win_x[-1], self.trunc_win_y[-1])
             print "Fluctuation window: (%d, %d) to (%d, %d)"%(self.fluc_win_x[0], self.fluc_win_y[0], self.fluc_win_x[-1], self.fluc_win_y[-1])
         return {'atom_number': atom_number,
                 'position_x':(self.distconv(coefs_x[1], axis=0)