c816d7012bad08d6dc94dce9ac085c36b32dc0b9
Will Turner <rwturner@stanford.edu>
2014-10-24 18:44:50 -0700

    Added Damping Calculation

--- BECphysics.py
+++ BECphysics.py
@@ -33,3 +33,18 @@ def temperature(tofs, widths):
     plt.plot(tofs, widths, '.'); plt.plot(xaxis, np.sqrt(slope*(xaxis*1e3)**2 + intercept*1e12)); 
     plt.xlabel('TOF / ms'); plt.ylabel('Sigma_x / um'); plt.title('Temperature Fit');plt.show()
     return (coefs[0] * M / KB, coefs)
+
+def tstar(n0):
+    '''Calculate characteristic phonon temperature, using eqn 10.78 in P&S pg
+    312'''
+    return (n0 * U0 / KB)
+
+def damping(n0, T):
+    '''Formulae from Pethick and Smith pg 313'''
+    Tstar = tstar(n0)
+    if 0.1*Tstar < T < 10*Tstar:
+        print "DANGER ZONE!"
+    if T < Tstar:
+        return (3 *  pi**4.5 / 5) * (n0 * A**3)**0.5 * (T / Tstar)**4 * omega_q
+    else:
+        return 2.09 * (A / n0)**0.5 * (omega_q / lambda_T**2)
