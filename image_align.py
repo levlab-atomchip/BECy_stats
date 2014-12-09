2138ef4fb9ce797c7d701ba55672ac63bde27b64
Richard Turner <rwturner@stanford.edu>
2014-12-08 19:27:26 -0800

    Added image alignment to resolution analysis

---
+++ image_align.py
@@ -0,0 +1,25 @@
+"""Image Alignment
+
+functions for alignment of atomic density images"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from cloud_distribution import *
+from cloud_image import CloudImage as ci
+import BECphysics as bp
+import bfieldsensitivity as bf
+
+def optimal_shift(img, ref_img, shift_range=range(-30,30)):
+    s1 = img
+    s2 = ref_img
+    shifts = np.array(shift_range)
+    msds = np.zeros(len(shifts))
+    for ii, sh in enumerate(shifts):
+        s1_shift = np.roll(s1, sh)
+        if sh != 0:
+            msds[ii] = np.mean(((s1_shift - s2)**2)[abs(sh):-abs(sh)])
+        else:
+            msds[ii] = np.mean((s1_shift - s2)**2)
+    bestshift = shifts[np.argmin(msds)]
+    return bestshift
