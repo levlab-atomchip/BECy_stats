15da70e79b0ecda71092dfb58debed2870e60131
atomchip_levlab <levlab.atomchip@gmail.com>
2015-01-22 12:29:43 -0800

    intensity noise analysis

--- intensity_noise.py
+++ intensity_noise.py
@@ -7,8 +7,8 @@ def image_subtract(im1, im2):
     return np.int32(im1) - np.int32(im2)
 
 def get_intensity_noise(img, num_bins=50, threshold = 0):
-    mean_img = image_subtract(0.5 * (img.atom_image_trunc + img.light_image_trunc), img.dark_image_trunc)
-    diff = image_subtract(img.light_image_trunc, img.atom_image_trunc)
+    mean_img = image_subtract(0.5 * (img.atom_image_trunc + img.fluc_cor*img.light_image_trunc), img.dark_image_trunc)
+    diff = image_subtract(img.fluc_cor*img.light_image_trunc, img.atom_image_trunc)
 
     mean_img_hist, mean_img_bins = np.histogram(np.ravel(mean_img), num_bins)
 
@@ -26,7 +26,7 @@ def get_intensity_noise(img, num_bins=50, threshold = 0):
     return mean_img_bins[:-1], filtered_bin_vars
 
 if __name__ == "__main__":
-    datadir = r'/home/will/levlab/data/intensity_noise_20150107/'
+    datadir = r'Z:\Data\ACM Data\Imaging system\PIXIS\2015-01-21\100us'
     dist = CDna(datadir, False)
     most_counts = 0
     sat_counts = 3400
@@ -35,13 +35,13 @@ if __name__ == "__main__":
         this_img = nai(fname)
         this_img.truncate_image(396, 623, 130, 148)
         this_bins, this_vars = get_intensity_noise(this_img)
-        this_sat = this_bins / sat_counts
+        this_sat = this_bins 
         plt.plot(this_sat, this_vars, '.', color='b')
         most_counts = max((most_counts, max(this_bins)))
-    plt.plot([0, float(most_counts)/sat_counts], [0, 2.0*most_counts], color='red', linewidth=3, label='Shot Noise Limited')
-    plt.xlim(0, 2)
-    plt.ylim(0, 30000)
-    plt.xlabel('Saturation')
+    plt.plot([0, float(most_counts)], [0, 2.0*most_counts], color='red', linewidth=3, label='Shot Noise Limited')
+   # plt.xlim(0, 2)
+   # plt.ylim(0, 30000)
+    plt.xlabel('Photon Counts')
     plt.ylabel('Variance')
     plt.legend(loc='upper left')
     plt.show(block=True)
